abeddd6c4b310c6ce7c80e7e9075924db5eecc29
Steffen Jaeckel <s@jaeckel.eu>
2012-11-23 03:02:50 +0100

    add hkdf_test()

--- src/headers/tomcrypt_hkdf.h
+++ src/headers/tomcrypt_hkdf.h
@@ -3,6 +3,8 @@
 /* ===> LTC_HKDF -- RFC5869 HMAC-based Key Derivation Function <=== */
 #ifdef LTC_HKDF
 
+int hkdf_test(void);
+
 int hkdf_extract(int hash_idx,
                  const unsigned char *salt, unsigned long saltlen,
                  const unsigned char *in,   unsigned long inlen,
