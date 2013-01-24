2cb8c441131585ca67a2133dce264f777d9e1f9d
Christopher Brown <cbrown@bittorrent.com>
2013-01-24 11:14:05 -0800

    der fixes and additions

--- src/pk/asn1/der/sequence/der_decode_subject_public_key_info.c
+++ src/pk/asn1/der/sequence/der_decode_subject_public_key_info.c
@@ -36,7 +36,8 @@ int der_decode_subject_public_key_info(const unsigned char *in, unsigned long in
         unsigned int algorithm, void* public_key, unsigned long* public_key_len,
         unsigned long parameters_type, ltc_asn1_list* parameters, unsigned long parameters_len)
 {
-   int err, len;
+   int err;
+   unsigned long len;
    oid_st oid;
    unsigned char *tmpbuf;
    unsigned long  tmpoid[16];
