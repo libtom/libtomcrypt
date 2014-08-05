98e05b10a00a295caee3299f0ee9d3861d87a796
Steffen Jaeckel <s@jaeckel.eu>
2014-08-05 23:58:59 +0200

    trim trailing spaces in mingw makefiles

--- updatemakes.sh
+++ updatemakes.sh
@@ -19,7 +19,8 @@ sed -e 's/ *$//' < tmp.delme > makefile.unix
 rm -f tmp.delme
 
 perl filter.pl makefile.mingw tmplist
-mv -f tmp.delme makefile.mingw
+sed -e 's/ *$//' < tmp.delme > makefile.mingw
+rm -f tmp.delme
 
 perl filter.pl makefile.msvc tmplist
 sed -e 's/\.o /.obj /g' -e 's/ *$//' < tmp.delme > makefile.msvc
