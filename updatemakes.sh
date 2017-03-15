9babf374ee0ca89c3acf27c89b26507068abde2d
Steffen Jaeckel <s@jaeckel.eu>
2017-03-15 22:21:53 +0100

    add '-c' option (again) to updatemakes.sh

--- updatemakes.sh
+++ updatemakes.sh
@@ -2,4 +2,11 @@
 
 ./helper.pl --update-makefiles || exit 1
 
+makefiles=(makefile makefile.icc makefile.shared makefile.unix makefile.mingw makefile.msvc)
+vcproj=(libtomcrypt_VS2008.vcproj libtomcrypt_VS2005.vcproj)
+
+if [ $# -eq 1 ] && [ "$1" == "-c" ]; then
+  git add ${makefiles[@]} ${vcproj[@]} && git commit -m 'Update makefiles'
+fi
+
 exit 0
