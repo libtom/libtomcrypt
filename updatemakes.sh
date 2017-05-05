3176103c156cdb4672c8d84f28c444822852f16c
Karel Miko <karel.miko@gmail.com>
2017-05-05 10:35:36 +0200

    fix makefile.icc in makefile update scripts

--- updatemakes.sh
+++ updatemakes.sh
@@ -2,7 +2,7 @@
 
 ./helper.pl --update-makefiles || exit 1
 
-makefiles=(makefile makefile.icc makefile.shared makefile.unix makefile.mingw makefile.msvc)
+makefiles=(makefile makefile.shared makefile.unix makefile.mingw makefile.msvc)
 vcproj=(libtomcrypt_VS2008.vcproj libtomcrypt_VS2005.vcproj)
 
 if [ $# -eq 1 ] && [ "$1" == "-c" ]; then
