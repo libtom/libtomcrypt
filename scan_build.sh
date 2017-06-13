ff2de0b07a98709cb5774d4db378698a2b5357a8
Steffen Jaeckel <s@jaeckel.eu>
2017-06-13 15:50:49 +0200

    looks like precise doesn't have scan-build under that name
    ...but we should be able to find it somewhere in /usr/bin/

--- scan_build.sh
+++ scan_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-[ "$TRAVIS_CI" != "" ] && sudo apt-get install clang -y -qq || true
+[ "$TRAVIS_CI" != "" ] && { [ -z "$(which scan-build)" ] && { echo "installing clang"; sudo apt-get install clang -y -qq; }; } || true
 
 # output version
 bash printinfo.sh
 
 make clean > /dev/null
 
-scan-build make -f makefile.unix all
+scan_build=$(which scan-build)
+[ -z "$scan_build" ] && scan_build=$(find /usr/bin/ -name 'scan-build-*' | sort -nr | head -n1) || true
+[ -z "$scan_build" ] && { echo "couldn't find clang scan-build"; exit 1; } || true
+$scan_build make -f makefile.unix all
