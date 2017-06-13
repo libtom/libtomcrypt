9baba0a83aca7e430f2a7ccdd54dedf208b302ad
Steffen Jaeckel <s@jaeckel.eu>
2017-06-13 17:21:46 +0200

    fix usage of makefile.unix

--- scan_build.sh
+++ scan_build.sh
@@ -9,4 +9,4 @@ make clean > /dev/null
 scan_build=$(which scan-build)
 [ -z "$scan_build" ] && scan_build=$(find /usr/bin/ -name 'scan-build-*' | sort -nr | head -n1) || true
 [ -z "$scan_build" ] && { echo "couldn't find clang scan-build"; exit 1; } || true
-$scan_build make -f makefile.unix all
+$scan_build make -f makefile.unix all CFLAGS="" EXTRALIBS=""
