e9c90e7f63eb25bb6751fd96cfdd074ff1710af8
Steffen Jaeckel <s@jaeckel.eu>
2017-06-22 08:38:23 +0200

    no need to use `XMEM_NEQ()` in PK crypto

--- src/pk/rsa/rsa_verify_hash.c
+++ src/pk/rsa/rsa_verify_hash.c
@@ -155,15 +155,15 @@ int rsa_verify_hash_ex(const unsigned char *sig,      unsigned long siglen,
       /* test OID */
       if ((reallen == outlen) &&
           (digestinfo[0].size == hash_descriptor[hash_idx].OIDlen) &&
-        (XMEM_NEQ(digestinfo[0].data, hash_descriptor[hash_idx].OID, sizeof(unsigned long) * hash_descriptor[hash_idx].OIDlen) == 0) &&
+        (XMEMCMP(digestinfo[0].data, hash_descriptor[hash_idx].OID, sizeof(unsigned long) * hash_descriptor[hash_idx].OIDlen) == 0) &&
           (siginfo[1].size == hashlen) &&
-        (XMEM_NEQ(siginfo[1].data, hash, hashlen) == 0)) {
+        (XMEMCMP(siginfo[1].data, hash, hashlen) == 0)) {
          *stat = 1;
       }
     } else {
       /* only check if the hash is equal */
       if ((hashlen == outlen) &&
-          (XMEM_NEQ(out, hash, hashlen) == 0)) {
+          (XMEMCMP(out, hash, hashlen) == 0)) {
         *stat = 1;
       }
     }
