68bf547f9c46aa537564f6af896670687cfb46f5
Steffen Jaeckel <s@jaeckel.eu>
2017-07-09 16:02:11 +0200

    use rsa_free() on error in rsa_make_key()

--- src/pk/rsa/rsa_make_key.c
+++ src/pk/rsa/rsa_make_key.c
@@ -97,7 +97,7 @@ int rsa_make_key(prng_state *prng, int wprng, int size, long e, rsa_key *key)
    err       = CRYPT_OK;
    goto cleanup;
 errkey:
-   mp_clear_multi(key->q, key->p, key->qP, key->dP, key->dQ, key->N, key->d, key->e, NULL);
+   rsa_free(key);
 cleanup:
    mp_clear_multi(tmp3, tmp2, tmp1, q, p, NULL);
    return err;
