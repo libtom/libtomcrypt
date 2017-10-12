58b731bc40769132c3106398d4f71679694eae8a
Steffen Jaeckel <s@jaeckel.eu>
2017-10-12 16:52:12 +0200

    use correct set of parameters

--- scan_build.sh
+++ scan_build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 if [ "$TRAVIS_CI" != "" ] && [ -z "$(echo $CC | grep "clang")" ]; then
     echo "no clang detected, early exit success"
     exit 0
@@ -17,4 +20,4 @@ make clean > /dev/null
 scan_build=$(which scan-build)
 [ -z "$scan_build" ] && scan_build=$(find /usr/bin/ -name 'scan-build-*' | sort -nr | head -n1) || true
 [ -z "$scan_build" ] && { echo "couldn't find clang scan-build"; exit 1; } || echo "run $scan_build"
-$scan_build --status-bugs make -f makefile.unix all CFLAGS="$2" EXTRALIBS="$5"
+$scan_build --status-bugs make all CFLAGS="$2 $CFLAGS $4" EXTRALIBS="$5"
