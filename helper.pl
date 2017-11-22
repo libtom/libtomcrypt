dd868600b373b8b8857a9348db4bb5aa3437434a
Karel Miko <karel.miko@gmail.com>
2017-11-22 18:27:11 +0100

    fix staticfunc_name detection
    (cherry picked from commit 6d71d657ef0371ac50c51ef9001e2e340dedf95a)

--- helper.pl
+++ helper.pl
@@ -61,7 +61,7 @@ sub check_source {
           $file !~ m|src/hashes/.*\.c$| &&
           $file !~ m|src/math/.+_desc.c$| &&
           $file !~ m|src/stream/sober128/sober128_stream.c$| &&
-          $l =~ /^static(\s+\S+)+\s+([^_][a-zA-Z0-9_]+)\s*\(/) {
+          $l =~ /^static(\s+[a-zA-Z0-9_]+)+\s+([^_][a-zA-Z0-9_]+)\s*\(/) {
         push @{$troubles->{staticfunc_name}}, "$lineno($2)";
       }
       $lineno++;
