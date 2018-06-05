ac1622e0421afede179f08ba387cac2f84f8ed0e
Karel Miko <karel.miko@gmail.com>
2018-06-05 16:39:38 +0200

    fix der_length_custom_type - incorrect length of length

--- src/pk/asn1/der/custom_type/der_length_custom_type.c
+++ src/pk/asn1/der/custom_type/der_length_custom_type.c
@@ -190,7 +190,7 @@ int der_length_custom_type(const ltc_asn1_list *root, unsigned long *outlen, uns
       }
    } else {
       /* calc length of length */
-      if ((err = der_length_asn1_length(y, &x)) != CRYPT_OK) {
+      if ((err = der_length_asn1_length(y - id_len, &x)) != CRYPT_OK) {
          goto LBL_ERR;
       }
       if (payloadlen != NULL) {
