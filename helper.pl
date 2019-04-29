a9a010d5fce01d683ce56df95e9488e595d64128
Adrian Antonana <mrpelotazo@posteo.net>
2019-04-29 18:30:30 +0200

    introduce initial cmake support

--- helper.pl
+++ helper.pl
@@ -279,6 +279,26 @@ sub version_from_tomcrypt_h {
   }
 }
 
+sub make_sources_cmake {
+  my ($list, $pub_headers) = @_;
+  my $output = "set(SOURCES\n";
+
+  foreach my $obj (sort @$list) {
+    $output .= $obj . "\n";
+  }
+
+  $output .= ")\n\nset(PUBLIC_HEADERS\n";
+
+  foreach my $obj (sort @$pub_headers) {
+    $output .= $obj . "\n";
+  }
+
+  $output .= ")\n\n";
+  $output .= "set_property(GLOBAL PROPERTY PUBLIC_HEADERS \$\{PUBLIC_HEADERS\}\)\n\n";
+
+  return $output;
+}
+
 sub process_makefiles {
   my $write = shift;
   my $changed_count = 0;
@@ -315,10 +335,12 @@ sub process_makefiles {
   }
 
   # update OBJECTS + HEADERS in makefile*
-  for my $m (qw/ makefile makefile.shared makefile.unix makefile.mingw makefile.msvc makefile_include.mk doc\/Doxyfile /) {
+  for my $m (qw/ makefile makefile.shared makefile.unix makefile.mingw makefile.msvc makefile_include.mk doc\/Doxyfile sources.cmake /) {
     my $old = read_file($m);
     my $new = $m eq 'makefile.msvc' ? patch_file($old, $var_obj, $var_h, $var_tobj, @ver_version)
-                                    : patch_file($old, $var_o, $var_h, $var_to, @ver_version);
+            : $m eq 'sources.cmake' ? make_sources_cmake(\@all, \@h)
+            :                         patch_file($old, $var_o, $var_h, $var_to, @ver_version);
+
     if ($old ne $new) {
       write_file($m, $new) if $write;
       warn "changed: $m\n";
