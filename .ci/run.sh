e2f5d17dcca819cfe2d322e58c347b0fb9d3f0df
Steffen Jaeckel <s@jaeckel.eu>
2019-09-24 15:34:43 +0200

    test default-build as first

--- .ci/run.sh
+++ .ci/run.sh
@@ -3,17 +3,7 @@
 # output version
 bash .ci/printinfo.sh
 
-bash .ci/build.sh " $1" "$2 -O2" "$3 IGNORE_SPEED=1" "$4" "$5"
-if [ -a testok.txt ] && [ -f testok.txt ]; then
-   echo
-else
-   echo
-   echo "Test failed"
-   exit 1
-fi
-
-rm -f testok.txt
-bash .ci/build.sh " $1" "$2 -Os" "$3 IGNORE_SPEED=1 LTC_SMALL=1" "$4" "$5"
+bash .ci/build.sh " $1" "$2" "$3" "$4" "$5"
 if [ -a testok.txt ] && [ -f testok.txt ]; then
    echo
 else
@@ -33,7 +23,17 @@ else
 fi
 
 rm -f testok.txt
-bash .ci/build.sh " $1" "$2" "$3" "$4" "$5"
+bash .ci/build.sh " $1" "$2 -O2" "$3 IGNORE_SPEED=1" "$4" "$5"
+if [ -a testok.txt ] && [ -f testok.txt ]; then
+   echo
+else
+   echo
+   echo "Test failed"
+   exit 1
+fi
+
+rm -f testok.txt
+bash .ci/build.sh " $1" "$2" "$3 IGNORE_SPEED=1 LTC_SMALL=1" "$4" "$5"
 if [ -a testok.txt ] && [ -f testok.txt ]; then
    echo
 else
