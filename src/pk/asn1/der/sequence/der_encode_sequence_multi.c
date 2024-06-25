9c1e83b51b53046c33caa4e47e40ccf0aed4d199
Etienne Carriere <etienne.carriere@foss.st.com>
2024-06-25 17:14:21 +0200

    fix XCALLOC() against GCC 14 -Wcalloc-transposed-args
    Fix use of XCALLOC() macro against GCC 14 directive
    -Wcalloc-transposed-args that makes GCC to complain with an warning/error
    trace message like the below when 1st argument is given by sizeof().
    
    warning: 'calloc' sizes specified with 'sizeof' in the earlier argument and not in the later argument [-Wcalloc-transposed-args]
    
    No functional changes.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- src/pk/asn1/der/sequence/der_encode_sequence_multi.c
+++ src/pk/asn1/der/sequence/der_encode_sequence_multi.c
@@ -80,7 +80,7 @@ int der_encode_sequence_multi(unsigned char *out, unsigned long *outlen, ...)
       return CRYPT_NOP;
    }
 
-   list = XCALLOC(sizeof(*list), x);
+   list = XCALLOC(x, sizeof(*list));
    if (list == NULL) {
       return CRYPT_MEM;
    }
