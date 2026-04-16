22201471191d83472ad2453655b9bf43a535569d
Karel Miko <karel.miko@gmail.com>
2026-04-17 00:44:51 +0200

    remove unnecessary #include <wchar.h> (we already handle it in tomcrypt_private.h)

--- src/pk/asn1/x509/x509_import.c
+++ src/pk/asn1/x509/x509_import.c
@@ -1,7 +1,6 @@
 /* LibTomCrypt, modular cryptographic library -- Tom St Denis */
 /* SPDX-License-Identifier: Unlicense */
 #include "tomcrypt_private.h"
-#include <wchar.h>
 
 /**
   @file x509_import.c
