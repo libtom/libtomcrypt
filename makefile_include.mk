be7cc72506b25325172f43f6385f903fbc26e213
Karel Miko <karel.miko@gmail.com>
2026-04-17 09:12:45 +0200

    CI tests (non-cmake) require to build demo x509_verify

--- makefile_include.mk
+++ makefile_include.mk
@@ -180,7 +180,7 @@ TEST=test
 USEFUL_DEMOS   = hashsum
 
 # Demos that are usable but only rarely make sense to be installed
-USEABLE_DEMOS  = aesgcm constants crypt der_print_flexi latex-tables openssh-privkey openssl-enc sizes timing
+USEABLE_DEMOS  = aesgcm constants crypt der_print_flexi latex-tables openssh-privkey openssl-enc sizes timing x509_verify
 
 # Demos that are used for testing or measuring
 TEST_DEMOS     = small tv_gen
