b0c3227c00bfb1d78edd605e2e7e033f5dd8afe3
Marek Knápek <knapek.mar@gmail.com>
2026-05-10 10:26:00 +0200

    LTC_UNUSED_PARAM

--- src/hashes/sha2/sha224_desc.c
+++ src/hashes/sha2/sha224_desc.c
@@ -124,8 +124,8 @@ int sha224_test(void)
 int sha224_test_desc(const struct ltc_hash_descriptor *desc, const char *name)
 {
  #ifndef LTC_TEST
-   (void)desc;
-   (void)name;
+   LTC_UNUSED_PARAM(desc);
+   LTC_UNUSED_PARAM(name);
    return CRYPT_NOP;
  #else
   static const struct {
